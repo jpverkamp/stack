6db8d6efa76e385657178f1b3c8972088471528f
JP Verkamp <me@jverkamp.com>
2023-04-10 17:34:38 -0400

    Implement if (todo: fix duplication)

--- src/vm.rs
+++ src/vm.rs
@@ -20,6 +20,25 @@ macro_rules! numeric_binop {
     }};
 }
 
+/// A helper macro to generate functions that operate on two integers and floats
+macro_rules! comparison_binop {
+    ($stack:expr, $f:expr) => {{
+        // TODO: Check we have enough values
+        let b = $stack.pop().unwrap();
+        let a = $stack.pop().unwrap();
+
+        let result = match (a, b) {
+            (Value::Integer(a), Value::Integer(b)) => Value::Boolean($f(a, b)),
+            (Value::Float(a), Value::Float(b)) => Value::Boolean($f(a, b)),
+            (Value::Integer(a), Value::Float(b)) => Value::Boolean($f(a as f64, b)),
+            (Value::Float(a), Value::Integer(b)) => Value::Boolean($f(a, b as f64)),
+            _ => unimplemented!(),
+        };
+
+        $stack.push(result);
+    }};
+}
+
 /// Evaluates a vector of expressions
 /// This does not actually return anything, but instead mutates the stack
 pub fn evaluate(ast: Expression) {
@@ -37,9 +56,58 @@ pub fn evaluate(ast: Expression) {
                 "*" => numeric_binop!(stack, |a, b| { a * b }),
                 "/" => numeric_binop!(stack, |a, b| { a / b }),
                 "%" => numeric_binop!(stack, |a, b| { a % b }),
+                "<" => comparison_binop!(stack, |a, b| { a < b }),
+                "<=" => comparison_binop!(stack, |a, b| { a <= b }),
+                "==" => comparison_binop!(stack, |a, b| { a == b }),
+                ">=" => comparison_binop!(stack, |a, b| { a >= b }),
+                ">" => comparison_binop!(stack, |a, b| { a > b }),
                 "writeln" => {
                     println!("{}", stack.pop().unwrap());
                 },
+                "if" => {
+                    let condition = stack.pop().unwrap();
+                    let false_branch = stack.pop().unwrap();
+                    let true_branch = stack.pop().unwrap();
+
+                    log::debug!("if condition: {}, true: {}, false: {}", condition, true_branch, false_branch);
+
+                    let branch = match condition {
+                        Value::Boolean(value) => if value { true_branch } else { false_branch },
+                        _ => panic!("if condition must be a boolean, got {}", condition),
+                    };
+
+                    log::debug!("if selected: {}", branch);
+                    
+                    match branch {
+                        // Blocks get evaluated lazily (now)
+                        Value::Block { arity_in, arity_out, expression } => {
+                            // TODO: Refactor this
+
+                            // Extend the stack with arity_in values
+                            let mut substack = stack.extend(arity_in);                           
+                            log::debug!("substack: {}", substack);
+
+                            // Evaluate the block with that new stack context
+                            eval(expression.as_ref().clone(), &mut substack);
+                            log::debug!("substack after eval: {}", substack);
+                            
+                            // Copy arity_out values to return, drop the rest of the substack
+                            // TODO: should this be a stack method?
+                            let mut results = vec![];
+                            for _ in 0..arity_out {
+                                results.push(substack.pop())
+                            }
+
+                            for result in results {
+                                stack.push(result.unwrap());
+                            }
+                        },
+                        // All literal values just get directly pushed
+                        _ => {
+                            stack.push(branch);
+                        }
+                    }
+                }
                 name => {
                     if let Some(value) = stack.get_named(String::from(name)) {
                         if let Value::Block { arity_in, arity_out, expression } = value {
