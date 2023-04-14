f6a150da80a6ffad595150979250734265e1388b
JP Verkamp <me@jverkamp.com>
2023-04-14 16:31:33 -0400

    Test cases for lexer, along with fixing based on order

--- src/lexer.rs
+++ src/lexer.rs
@@ -14,16 +14,18 @@ pub fn tokenize(reader: impl BufRead) -> Vec<Token> {
         // single characters
         r"[\{}()\[\]]",
         // <numbers>
-        // integers
-        r"-?\d+(\.\d*)?",
-        // rationals
-        r"-?\d+(\.\d*)?/\d+(\.\d*)?",
-        // floats (including scientific notation)
-        r"-?\d+(\.\d*)?[eE]-?\d+(\.\d*)?",
         // complex numbers
         r"-?\d+(\.\d*)?[+-]-?\d+(\.\d*)?i",
+        // floats (including scientific notation)
+        r"-?\d+(\.\d*)?[eE]-?\d+(\.\d*)?",
+        // rationals
+        r"-?\d+/\d+",
         // hex literals
         r"0x[0-9a-fA-F]+",
+        // binary literals
+        r"0b[01]+",
+        // integers
+        r"-?\d+(\.\d*)?",
         // </numbers>
         // strings
         "\"(\\.|[^\"])*\"",
@@ -77,4 +79,154 @@ pub fn tokenize(reader: impl BufRead) -> Vec<Token> {
     }
 
     tokens
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_brackets() {
+        let input = "[](){}";
+        let tokens = super::tokenize(input.as_bytes());
+        assert_eq!(tokens.len(), 6);
+        assert_eq!(tokens[0].token, "[");
+        assert_eq!(tokens[1].token, "]");
+        assert_eq!(tokens[2].token, "(");
+        assert_eq!(tokens[3].token, ")");
+        assert_eq!(tokens[4].token, "{");
+        assert_eq!(tokens[5].token, "}");
+    }
+
+    #[test]
+    fn test_integers() {
+        let input = "1 2 3 100 8675309 0";
+        let tokens = super::tokenize(input.as_bytes());
+        assert_eq!(tokens.len(), 6);
+        assert_eq!(tokens[0].token, "1");
+        assert_eq!(tokens[1].token, "2");
+        assert_eq!(tokens[2].token, "3");
+        assert_eq!(tokens[3].token, "100");
+        assert_eq!(tokens[4].token, "8675309");
+        assert_eq!(tokens[5].token, "0");
+    }
+
+    #[test]
+    fn test_negative_integers() {
+        let input = "-1 -2 -3 -100 -8675309 -0";
+        let tokens = super::tokenize(input.as_bytes());
+        assert_eq!(tokens.len(), 6);
+        assert_eq!(tokens[0].token, "-1");
+        assert_eq!(tokens[1].token, "-2");
+        assert_eq!(tokens[2].token, "-3");
+        assert_eq!(tokens[3].token, "-100");
+        assert_eq!(tokens[4].token, "-8675309");
+        assert_eq!(tokens[5].token, "-0");
+    }
+
+    #[test]
+    fn test_rationals() {
+        let input = "1/2 3/4 5/6 7/8 9/10";
+        let tokens = super::tokenize(input.as_bytes());
+        assert_eq!(tokens.len(), 5);
+        assert_eq!(tokens[0].token, "1/2");
+        assert_eq!(tokens[1].token, "3/4");
+        assert_eq!(tokens[2].token, "5/6");
+        assert_eq!(tokens[3].token, "7/8");
+        assert_eq!(tokens[4].token, "9/10");
+    }
+
+    #[test]
+    fn test_floats() {
+        let input = "1.0 2.0 3.0 100.0 8675309.0 0.0";
+        let tokens = super::tokenize(input.as_bytes());
+        assert_eq!(tokens.len(), 6);
+        assert_eq!(tokens[0].token, "1.0");
+        assert_eq!(tokens[1].token, "2.0");
+        assert_eq!(tokens[2].token, "3.0");
+        assert_eq!(tokens[3].token, "100.0");
+        assert_eq!(tokens[4].token, "8675309.0");
+        assert_eq!(tokens[5].token, "0.0");
+    }
+
+    #[test]
+    fn test_float_scientific() {
+        let input = "1e1 1.1e2 1.0e3 1.0e2 8.6e6 0e0";
+        let tokens = super::tokenize(input.as_bytes());
+        assert_eq!(tokens.len(), 6);
+        assert_eq!(tokens[0].token, "1e1");
+        assert_eq!(tokens[1].token, "1.1e2");
+        assert_eq!(tokens[2].token, "1.0e3");
+        assert_eq!(tokens[3].token, "1.0e2");
+        assert_eq!(tokens[4].token, "8.6e6");
+        assert_eq!(tokens[5].token, "0e0");
+    }
+
+    #[test]
+    fn test_hex() {
+        let input = "0x1 0xFF 0xdeadbeef 0x0";
+        let tokens = super::tokenize(input.as_bytes());
+        assert_eq!(tokens.len(), 4);
+        assert_eq!(tokens[0].token, "0x1");
+        assert_eq!(tokens[1].token, "0xFF");
+        assert_eq!(tokens[2].token, "0xdeadbeef");
+        assert_eq!(tokens[3].token, "0x0");
+    }
+
+    #[test]
+    fn test_binary() {
+        let input = "0b1 0b1111 0b1101 0b0";
+        let tokens = super::tokenize(input.as_bytes());
+        assert_eq!(tokens.len(), 4);
+        assert_eq!(tokens[0].token, "0b1");
+        assert_eq!(tokens[1].token, "0b1111");
+        assert_eq!(tokens[2].token, "0b1101");
+        assert_eq!(tokens[3].token, "0b0");
+    }
+
+    #[test]
+    fn test_strings() {
+        let input = "\"\" \"hello\" \"hello world\" \"hello\\nworld\"";
+        let tokens = super::tokenize(input.as_bytes());
+        assert_eq!(tokens.len(), 4);
+        assert_eq!(tokens[0].token, "\"\"");
+        assert_eq!(tokens[1].token, "\"hello\"");
+        assert_eq!(tokens[2].token, "\"hello world\"");
+        assert_eq!(tokens[3].token, "\"hello\\nworld\"");
+    }
+
+    #[test]
+    fn test_identifiers() {
+        let input = "test fact camelCase snake_case";
+        let tokens = super::tokenize(input.as_bytes());
+        assert_eq!(tokens.len(), 4);
+        assert_eq!(tokens[0].token, "test");
+        assert_eq!(tokens[1].token, "fact");
+        assert_eq!(tokens[2].token, "camelCase");
+        assert_eq!(tokens[3].token, "snake_case");
+    }
+
+    #[test]
+    fn test_symbolic() {
+        let input = "+ - * &! | ^ ~!! <==";
+        let tokens = super::tokenize(input.as_bytes());
+        assert_eq!(tokens.len(), 8);
+        assert_eq!(tokens[0].token, "+");
+        assert_eq!(tokens[1].token, "-");
+        assert_eq!(tokens[2].token, "*");
+        assert_eq!(tokens[3].token, "&!");
+        assert_eq!(tokens[4].token, "|");
+        assert_eq!(tokens[5].token, "^");
+        assert_eq!(tokens[6].token, "~!!");
+        assert_eq!(tokens[7].token, "<==");
+    }
+
+    #[test]
+    fn test_prefixed() {
+        let input = "@fact !fact";
+        let tokens = super::tokenize(input.as_bytes());
+        assert_eq!(tokens.len(), 4);
+        assert_eq!(tokens[0].token, "@");
+        assert_eq!(tokens[1].token, "fact");
+        assert_eq!(tokens[2].token, "!");
+        assert_eq!(tokens[3].token, "fact");
+    }
 }
