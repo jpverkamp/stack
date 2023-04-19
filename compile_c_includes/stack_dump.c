5c2b241fd0a92d2972577f4cc38caf6ba2509bb2
JP Verkamp <me@jverkamp.com>
2023-04-19 13:08:44 -0400

    Add a debug function for dumping the current stack

---
+++ compile_c_includes/stack_dump.c
@@ -0,0 +1,38 @@
+void stack_dump()
+{
+    for (Value *ptr = stack; ptr < stack_ptr; ptr++)
+    {
+        if (ptr->type == TAG_NUMBER_INTEGER)
+        {
+            printf("%lld", ptr->as_integer);
+        }
+        else if (ptr->type == TAG_NUMBER_FLOAT)
+        {
+            printf("%f", ptr->as_float);
+        }
+        else if (ptr->type == TAG_STRING)
+        {
+            printf("%s", ptr->as_string);
+        }
+        else if (ptr->type == TAG_BOOLEAN)
+        {
+            printf(ptr->as_boolean ? "true" : "false");
+        }
+        else if (ptr->type == TAG_BLOCK)
+        {
+            printf("{block}");
+        }
+        else
+        {
+            // TODO: Error
+        }
+
+        for (int i = 0; i < ptr->name_count; i++)
+        {
+            printf("@%s", get_name(ptr->names[i]));
+        }
+
+        printf(" ");
+    }
+    printf("\n");
+}
