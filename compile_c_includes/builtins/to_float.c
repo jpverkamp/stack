23e9703288ac81d375a8eb8e431c05665230c3a7
JP Verkamp <jverkamp@dropbox.com>
2023-07-11 23:51:46 -0400

    Split manual numeric type casting (less necessary now that coerce works)

---
+++ compile_c_includes/builtins/to_float.c
@@ -0,0 +1,33 @@
+{
+    Value *v = stack_ptr;
+
+    if (v->type == TAG_NUMBER_INTEGER)
+    {
+        v->as_float = (double)v->as_integer;
+    }
+    else if (v->type == TAG_NUMBER_FLOAT)
+    {
+        // Do nothing, already a float
+    }
+    else if (v->type == TAG_STRING)
+    {
+        printf("todo: cast string to float");
+        exit(1);
+    }
+    else if (v->type == TAG_BOOLEAN)
+    {
+        v->as_float = v->as_boolean ? 1.0 : 0.0;
+    }
+    else if (v->type == TAG_BLOCK)
+    {
+        printf("error: cannot cast block to float");
+        exit(1);
+    }
+    else
+    {
+        printf("error: unknown type to cast to float");
+        exit(1);
+    }
+
+    v->type = TAG_NUMBER_FLOAT;
+}
