b37336ef6a04a09e8abcdc4adb33732192db469f
JP Verkamp <jverkamp@dropbox.com>
2023-08-04 21:34:53 -0400

    Fix copy/paste errors

--- compile_c_includes/stack_dump.c
+++ compile_c_includes/stack_dump.c
@@ -2,38 +2,38 @@ void stack_dump(Name *names)
 {
     if (stack_ptr == stack)
     {
-        ffprintf(stderr, stderr, " STACK: <empty>");
+        fprintf(stderr, " STACK: <empty>");
     }
     else
     {
-        ffprintf(stderr, stderr, " STACK: ");
+        fprintf(stderr, " STACK: ");
         for (Value *ptr = stack_ptr; ptr != stack; ptr--)
         {
             if (ptr->type == TAG_NUMBER_INTEGER)
             {
-                ffprintf(stderr, stderr, "%lld", ptr->as_integer);
+                fprintf(stderr, "%lld", ptr->as_integer);
             }
             else if (ptr->type == TAG_NUMBER_FLOAT)
             {
-                ffprintf(stderr, stderr, "%f", ptr->as_float);
+                fprintf(stderr, "%f", ptr->as_float);
             }
             else if (ptr->type == TAG_STRING)
             {
-                ffprintf(stderr, stderr, "%s", ptr->as_string);
+                fprintf(stderr, "%s", ptr->as_string);
             }
             else if (ptr->type == TAG_BOOLEAN)
             {
-                ffprintf(stderr, stderr, ptr->as_boolean ? "true" : "false");
+                fprintf(stderr, ptr->as_boolean ? "true" : "false");
             }
             else if (ptr->type == TAG_BLOCK)
             {
-                ffprintf(stderr, stderr, "{block}");
+                fprintf(stderr, "{block}");
             }
             else
             {
-                ffprintf(stderr, stderr, "{UNKNOWN}");
+                fprintf(stderr, "{UNKNOWN}");
             }
-            ffprintf(stderr, stderr, " ");
+            fprintf(stderr, " ");
         }
     }
 
