4f6c80fabd97daf66ba23867c5c71c9198ab264d
JP Verkamp <jverkamp@dropbox.com>
2024-12-07 09:58:39 -0500

    fmt

--- src/lexer.rs
+++ src/lexer.rs
@@ -13,6 +13,7 @@ pub fn tokenize(reader: impl BufRead) -> Vec<Token> {
     let token_patterns = vec![
         // single characters
         r"[\{}()\[\]]",
+        
         // <numbers>
         // complex numbers
         r"-?\d+(\.\d*)?[+-]-?\d+(\.\d*)?i",
